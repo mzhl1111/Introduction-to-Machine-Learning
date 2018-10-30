5c410f237861ce2e361dbbc0ae984a15d9f71914
TYTYTYTYTYTYTYTYTY <sllzhlv77@gmail.com>
2018-10-30 17:26:23 -0500

    finished

--- hw4/computeLegPoly.m
+++ hw4/computeLegPoly.m
@@ -6,3 +6,10 @@ function [ z ] = computeLegPoly( x, Q )
 %   Output:
 %       z: matrix where each column is the Legendre polynomials of order 0 
 %          to Q, evaluated at the corresponding x value in the input
+
+z = 0;
+for i = 0:Q
+    z = z+ nchoosek(Q,i)^2*(x-1)^(Q-i)*(x+1)^i/2^Q;
+end 
+z;
+end 
