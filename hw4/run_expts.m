540124a1808f89de80fc2a37933684d12de6d5aa
TYTYTYTYTYTYTYTYTY <sllzhlv77@gmail.com>
2018-11-01 08:37:00 -0500

    plot updated

--- hw4/run_expts.m
+++ hw4/run_expts.m
@@ -14,4 +14,27 @@ for ii = 1:length(Q_f)
     end
     fprintf('.');
 end
-            
+    
+
+% Same deterministic noise, different N and level of stochastic noise 
+
+subplot(2,2,1)
+plot(transpose(vertcat(expt_data_mat(1,:,1),expt_data_mat(1,:,2),expt_data_mat(1,:,3),expt_data_mat(1,:,4),expt_data_mat(1,:,5))))
+subplot(2,2,2)
+plot(transpose(vertcat(expt_data_mat(2,:,1),expt_data_mat(2,:,2),expt_data_mat(2,:,3),expt_data_mat(2,:,4),expt_data_mat(2,:,5))))
+subplot(2,2,3)
+plot(transpose(vertcat(expt_data_mat(3,:,1),expt_data_mat(3,:,2),expt_data_mat(3,:,3),expt_data_mat(3,:,4),expt_data_mat(3,:,5))))
+subplot(2,2,4)
+plot(transpose(vertcat(expt_data_mat(4,:,1),expt_data_mat(4,:,2),expt_data_mat(4,:,3),expt_data_mat(4,:,4),expt_data_mat(4,:,5))))
+
+% Same stochastic noise, different N and deterministic noise 
+subplot(3,2,[5,6])
+plot(transpose(expt_data_mat(:,:,5)))
+subplot(3,2,4)
+plot(transpose(expt_data_mat(:,:,4)))
+subplot(3,2,3)
+plot(transpose(expt_data_mat(:,:,3)))
+subplot(3,2,2)
+plot(transpose(expt_data_mat(:,:,2)))
+subplot(3,2,1)
+plot(transpose(expt_data_mat(:,:,1)))
