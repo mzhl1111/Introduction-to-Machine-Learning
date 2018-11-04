b2238c8fc2eb1195c35a4f3c4f00fb97e3829f2c
TYTYTYTYTYTYTYTYTY <sllzhlv77@gmail.com>
2018-11-04 10:45:46 -0600

    update
    try to skip loops

--- hw4/computeLegPoly.m
+++ hw4/computeLegPoly.m
@@ -7,9 +7,9 @@ function [ z ] = computeLegPoly( x, Q )
 %       z: matrix where each column is the Legendre polynomials of order 0 
 %          to Q, evaluated at the corresponding x value in the input
 
-z = 0;
+z = zeros(Q+1,length(x));
 for i = 0:Q
-    z = z+ nchoosek(Q,i)^2*(x-1)^(Q-i)*(x+1)^i/2^Q;
+    z(i+1,:) = nchoosek(Q,i)^2*(x-1).^(Q-i).*(x+1).^i/2^Q;
 end 
 z;
 end 
--- hw4/generate_dataset.m
+++ hw4/generate_dataset.m
@@ -10,30 +10,25 @@ function [ train_set test_set ] = generate_dataset( Q_f, N_train, N_test, sigma 
 %       train_set and test_set are both 2-column matrices in which each row
 %       represents an (x,y) pair
 
-train_x = zeros(N_train,1);
-train_y = zeros(N_train,1);
-train_a = normrnd(0,1,[1,Q_f+1]) / sum(sqrt(1./(1:2:(2*Q_f+2))));
+q_a = normrnd(0,1,[Q_f+1,1]) / sum(sqrt(1./(1:2:(2*Q_f+2))));
+
+xs = 2*rand(1,N_train+N_test);
+
+ys = sum(computeLegPoly(xs,Q_f) .* q_a);
+
+data = vertcat(xs,ys);
+
+train_set = transpose(data(:,1:N_train));
+
+test_set = transpose(data(:,(N_train+1): (N_test+N_train)));
 
 
 
-for i = 1:N_train
-    train_x(i) = 2*rand(1)-1;
-    for j = 0:Q_f
-        train_y(i)= train_y(i) + train_a(j+1)*computeLegPoly(train_x(i),j);
-    end  
-    train_y(i) = train_y(i)+sigma*normrnd(0,1);
-end 
 
-test_x = zeros(N_test,1);
-test_y = zeros(N_test,1);
-for i = 1:N_test
-    test_x(i) = 2*rand(1)-1;
-    for j = 0:Q_f
-        test_y(i)= test_y(i) + train_a(j+1)*computeLegPoly(test_x(i),j);
-    end  
-    test_y(i) = test_y(i)+sigma*normrnd(0,1);
-end 
-train_set = [train_x train_y];
-test_set = [test_x test_y];
+
+
+
+
+
 
 end
