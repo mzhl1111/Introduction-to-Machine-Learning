e2764ea075d6fa1c915a7b6afe989811f0c58175
TYTYTYTYTYTYTYTYTY <sllzhlv77@gmail.com>
2018-11-11 17:25:37 -0600

    new

---
+++ hw5/AdaBoost.m
@@ -0,0 +1,16 @@
+function [ train_err, test_err ] = AdaBoost( X_tr, y_tr, X_te, y_te, n_trees )
+%AdaBoost: Implement AdaBoost using decision stumps learned
+%   using information gain as the weak learners.
+%   X_tr: Training set
+%   y_tr: Training set labels
+%   X_te: Testing set
+%   y_te: Testing set labels
+%   n_trees: The number of trees to use
+
+
+
+
+
+
+end
+
---
+++ hw5/BaggedTrees.m
@@ -0,0 +1,13 @@
+function [ oobErr ] = BaggedTrees( X, Y, numBags )
+%BAGGEDTREES Returns out-of-bag classification error of an ensemble of
+%numBags CART decision trees on the input dataset, and also plots the error
+%as a function of the number of bags from 1 to numBags
+%   Inputs:
+%       X : Matrix of training data
+%       Y : Vector of classes of the training examples
+%       numBags : Number of trees to learn in the ensemble
+%
+%   You may use "fitctree" but do not use "TreeBagger" or any other inbuilt
+%   bagging function
+
+end
